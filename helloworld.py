f0d11510e70676d4c7a1973077205d7b4f970834
Jeremy Simmons <jsimmons@jeremysimmons.net>
2021-11-21 14:48:51 -0800

    v2

--- helloworld.py
+++ helloworld.py
@@ -1,13 +1,13 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 
-app = Flask(__name__)
-api = Api(app)
-
 class Greeting (Resource):
    def get(self):
-      return { "message" : "Hello Flask API World!" }
-api.add_resource(Greeting, '/') # Route_1
+      return { "message" : "Hello Flask API World!", "version": 2 }
+
+app = Flask(__name__)
+api = Api(app)
+api.add_resource(Greeting, '/')
 
 if __name__ == '__main__':
    app.run('0.0.0.0','8080')
