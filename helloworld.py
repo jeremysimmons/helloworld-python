e3eaef82e84410907e82577558b1c88b4a61d35c
Jeremy Simmons <jsimmons@jeremysimmons.net>
2021-11-21 14:49:15 -0800

    v3

--- helloworld.py
+++ helloworld.py
@@ -3,7 +3,7 @@ from flask_restful import Resource, Api
 
 class Greeting (Resource):
    def get(self):
-      return { "message" : "Hello Flask API World!", "version": 2 }
+      return { "message" : "Hello Flask API World!", "version": 3 }
 
 app = Flask(__name__)
 api = Api(app)
